c36519619d6b51cdc9c6dd97dd5963f71ec4cf6f
dmsqls0427k <danaeblee@gmail.com>
2017-05-08 22:05:32 -0400

    log

--- app/src/main/java/com/example/hyun/triptap/WelcomeActivity.java
+++ app/src/main/java/com/example/hyun/triptap/WelcomeActivity.java
@@ -1,13 +1,24 @@
 package com.example.hyun.triptap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    private Button guestButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        guestButton = (Button) findViewById(R.id.try_guest);
+        guestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            }
+        });
         setContentView(R.layout.activity_welcome);
     }
+
 }
