0e153907f61b5c88dff59b22c533c39105b89298
hpark <hobmobo@gmail.com>
2017-05-11 02:22:53 -0400

    AddTripActivity added

--- app/src/main/java/com/example/hyun/triptap/MainActivity.java
+++ app/src/main/java/com/example/hyun/triptap/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.hyun.triptap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addButton = (Button) findViewById(R.id.add_trip);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddTripActivity.class));
+            }
+        });
     }
 }
