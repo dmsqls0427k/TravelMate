ff7cb9908d10928772288d14f0bdbc4eb926648d
hpark <hobmobo@gmail.com>
2017-05-23 17:06:37 -0400

    From Calendar pop up

--- app/src/main/java/com/example/hyun/triptap/AddTripActivity.java
+++ app/src/main/java/com/example/hyun/triptap/AddTripActivity.java
@@ -1,13 +1,60 @@
 package com.example.hyun.triptap;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class AddTripActivity extends AppCompatActivity {
 
+    private EditText fromText, toText;
+    private Calendar fromCalendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trip);
+
+        fromText = (EditText) findViewById(R.id.edit_from);
+        toText = (EditText) findViewById(R.id.edit_to);
+
+        fromCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                fromCalendar.set(Calendar.YEAR, year);
+                fromCalendar.set(Calendar.MONTH, month);
+                fromCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        fromText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddTripActivity.this, date, fromCalendar
+                        .get(Calendar.YEAR), fromCalendar.get(Calendar.MONTH),
+                        fromCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        fromText.setText(sdf.format(fromCalendar.getTime()));
     }
 }
+
+
+
