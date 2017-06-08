7300ffbad58054973229703a1645548cebb4b8da
hpark <hobmobo@gmail.com>
2017-06-08 16:36:46 -0400

    From/To Date Constraints Added

--- app/src/main/java/com/example/hyun/triptap/AddTripActivity.java
+++ app/src/main/java/com/example/hyun/triptap/AddTripActivity.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.Locale;
 
 import android.app.DatePickerDialog;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,10 +31,36 @@ public class AddTripActivity extends AppCompatActivity {
 
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                fromCalendar.set(Calendar.YEAR, year);
-                fromCalendar.set(Calendar.MONTH, month);
-                fromCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel(fromText);
+                if (fromText.getText().toString().equals("")) {
+                    fromCalendar.set(Calendar.YEAR, year);
+                    fromCalendar.set(Calendar.MONTH, month);
+                    fromCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    updateLabel(fromText);
+                } else {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(AddTripActivity.this);
+                    alert.setTitle("Invalid selection");
+                    alert.setMessage("The date has to be before " + toText.getText());
+                    alert.setPositiveButton("OK", null);
+                    if (year <= toCalendar.get(Calendar.YEAR)) {
+                        fromCalendar.set(Calendar.YEAR, year);
+                        if (month > toCalendar.get(Calendar.MONTH)
+                                && year == toCalendar.get(Calendar.YEAR)) {
+                            alert.show();
+                        } else {
+                            fromCalendar.set(Calendar.MONTH, month);
+                            if (month == toCalendar.get(Calendar.MONTH)
+                                    && dayOfMonth > toCalendar.get(Calendar.DAY_OF_MONTH)) {
+                                alert.show();
+                            } else {
+                                fromCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                                updateLabel(fromText);
+                            }
+                        }
+                    } else {
+                        alert.show();
+                    }
+                }
+
             }
         };
 
@@ -50,10 +77,35 @@ public class AddTripActivity extends AppCompatActivity {
 
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                toCalendar.set(Calendar.YEAR, year);
-                toCalendar.set(Calendar.MONTH, month);
-                toCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel(toText);
+                if (toText.getText().toString().equals("")) {
+                    toCalendar.set(Calendar.YEAR, year);
+                    toCalendar.set(Calendar.MONTH, month);
+                    toCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    updateLabel(toText);
+                } else {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(AddTripActivity.this);
+                    alert.setTitle("Invalid selection");
+                    alert.setMessage("The date has to be after " + fromText.getText());
+                    alert.setPositiveButton("OK", null);
+                    if (year >= fromCalendar.get(Calendar.YEAR)) {
+                        toCalendar.set(Calendar.YEAR, year);
+                        if (month < fromCalendar.get(Calendar.MONTH)
+                                && year == fromCalendar.get(Calendar.YEAR)) {
+                            alert.show();
+                        } else {
+                            toCalendar.set(Calendar.MONTH, month);
+                            if (month == fromCalendar.get(Calendar.MONTH)
+                                    && dayOfMonth < fromCalendar.get(Calendar.DAY_OF_MONTH)) {
+                                alert.show();
+                            } else {
+                                toCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                                updateLabel(toText);
+                            }
+                        }
+                    } else {
+                        alert.show();
+                    }
+                }
             }
         };
 
@@ -61,6 +113,7 @@ public class AddTripActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+
                 new DatePickerDialog(AddTripActivity.this, toDate, toCalendar
                         .get(Calendar.YEAR), toCalendar.get(Calendar.MONTH),
                         toCalendar.get(Calendar.DAY_OF_MONTH)).show();
